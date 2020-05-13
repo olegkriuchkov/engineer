86007bf2391f7ce244eab07b40e19f8539ab3850
Oleg <okriuchkov@gmail.com>
2020-05-13 16:51:40 +0300

    Added ListItem

--- src/components/AsteriodInfo.js
+++ src/components/AsteriodInfo.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import { Redirect } from "react-router-dom";
-import { List, ListItemText } from '@material-ui/core';
+import { List, ListItemText, ListItem } from '@material-ui/core';
 
 class AsteroidInfo extends Component {
     componentWillUnmount() {
@@ -12,9 +12,15 @@ class AsteroidInfo extends Component {
         }
         return (
             <List className="asteroid-info">
-                <ListItemText>Name: {this.props.asteroidInfo? this.props.asteroidInfo.name : "asteroid name"}</ListItemText>
-                <ListItemText>nasa_jpl_url: {this.props.asteroidInfo? this.props.asteroidInfo.nasa_jpl_url : "asteroid nasa_jpl_url"}</ListItemText>
-                <ListItemText>is_potentially_hazardous_asteroid: {this.props.asteroidInfo? `${this.props.asteroidInfo.is_potentially_hazardous_asteroid}` : "is_potentially_hazardous_asteroid"}</ListItemText>
+                <ListItem>
+                    <ListItemText>Name: {this.props.asteroidInfo? this.props.asteroidInfo.name : "asteroid name"}</ListItemText>
+                </ListItem>
+                <ListItem>
+                    <ListItemText>nasa_jpl_url: {this.props.asteroidInfo? this.props.asteroidInfo.nasa_jpl_url : "asteroid nasa_jpl_url"}</ListItemText>
+                </ListItem>
+                <ListItem>
+                    <ListItemText>is_potentially_hazardous_asteroid: {this.props.asteroidInfo? `${this.props.asteroidInfo.is_potentially_hazardous_asteroid}` : "is_potentially_hazardous_asteroid"}</ListItemText>
+                </ListItem>
             </List>
         );
     }
